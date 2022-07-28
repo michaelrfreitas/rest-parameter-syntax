973f3f95da4cfd82a17b5743a117d94e05b8c9de
Michael Freitas <michael_rfreitas@hotmail.com>
2022-07-28 21:47:20 +0000

    rest parameter syntax

--- rest-parameter-syntax.js
+++ rest-parameter-syntax.js
@@ -4,7 +4,50 @@
  */
 
 // Regular function call 
+const sumAll = (a, b, c) => a + b + c;
+let sum = sumAll(1, 2, 3);
+console.log("Sum:", sum);
 
 // Extra arguments are ignored
+let sum2 = sumAll(1, 2, 3, 4, 5, 6);
+console.log("Sum2:", sum2);
 
 // Function using ...rest
+const sumRest = (a, b, c, ...rest) => {
+    let sum = a + b + c;
+    for (let i of rest) {
+        sum += i;
+    }
+    return sum;
+}
+
+let sum3 = sumRest(1, 2, 3, 4, 5, 6);
+console.log("Sum3:", sum3);
+
+
+let mixedLetters = ['b', 'd', 'a', 'c', 'f', 'e'];
+
+let moreMixedLetters = [...mixedLetters, 'h', 'k', 'g', 'j', 'i', 'l'];
+
+console.log(moreMixedLetters);
+
+/* How i envision students doing this challenge 
+
+const updateSortReverse =  (arr, ...letters) => {
+    let copy = [...arr];
+    for(let i of letters) {
+        copy.push(i);
+    }
+    
+    return copy.sort().reverse();
+};
+*/
+
+const updateSortReverse = (arr, ...letters) => [...arr, ...letters].sort().reverse();
+
+let reverseSort = updateSortReverse(moreMixedLetters, 'n', 'm', 'o');
+console.log(reverseSort);
+console.log(mixedLetters);
+
+
+
